974f1d17c733f0e43cae76b28b3f70fa263c0bf0
Erinhj <jin7903ok@gmail.com>
2025-08-20 17:13:17 +0900

    Remove unnecessary project folders from Git tracking

--- Assignment5 ver.2/src/ai/dev.ts
+++
@@ -1,7 +0,0 @@
-import { config } from 'dotenv';
-config();
-
-// This file is used for Genkit development and debugging.
-// It is not part of the Next.js application build.
-import './flows/narrative-variation';
-import './flows/generate-image';
